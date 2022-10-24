659165c91c66f14c4678e00609c331506a287138
alerodriargui <alerodriargui@gmail.com>
2022-10-24 23:19:47 +0200

    Se han añadido variables para entender problema

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -7,12 +7,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
 from practica1 import agent, joc
+from practica1 import entorn as entorn_practica1
 
 
 def main():
     rana = agent.Rana("Miquel")
     lab = joc.Laberint([rana], parets=True)
+    pos_rana = rana.get_rana()
+    pizza = lab.get_pizza()
+    print(pizza)    # Posición estado inicial pizza
+    print(pos_rana) # Posición estado inicial rana
     lab.comencar()
+
 
 
 if __name__ == "__main__":
