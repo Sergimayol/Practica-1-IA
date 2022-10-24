8499fc275fd1687325fab3086ebdd3a2e833d9e4
alerodriargui <alerodriargui@gmail.com>
2022-10-24 23:19:18 +0200

    Definición de clase Estado

--- practica1/agent.py
+++ practica1/agent.py
@@ -9,6 +9,7 @@ from ia_2022 import entorn
 from practica1 import joc, entorn as entorn_practica1
 
 
+
 class Rana(joc.Rana):
     def __init__(self, *args, **kwargs):
         super(Rana, self).__init__(*args, **kwargs)
@@ -23,3 +24,35 @@ class Rana(joc.Rana):
 
         # EN un futuro cambiar, de momento es para que no de error
         return entorn_practica1.AccionsRana.ESPERAR
+
+class Estado:
+    def __init__(self, info: str, coste: int, padre=None):
+        self.__info = info   #[posicio, olor, parets]
+        self.__padre = padre # Estado
+        self.__coste = coste # Coste de las acciones
+
+    def __hash__(self) -> int:
+        return hash(tuple(self.__info))
+
+    @property
+    def info(self):
+        return self.__info
+
+    def es_meta(self) -> bool:
+        return self.__info == entorn_practica1.ClauPercepcio.OLOR
+
+    def padre(self):
+        return self.__padre
+    
+    def __str__(self) -> str:
+        return str(self.__info)
+    
+    # Método para generar hijos de un estado
+    def generar_hijos(self):
+        hijos = [] # Lista de estados
+
+        # Primero para generar los hijos debemos saber el estado inicial y el estado final
+        # sabiendo el estado inicial de la pizza y el estado inicial de la rana, podemos calcular de la forma que nos convenga
+        # es decir, si por ejemplo tenemos la pizza en la posición (0,0) y la rana en la posición (1,1) entonces sabemos que la rana debe igualar la posición de la pizza
+        # y hará los movimientos basados en el algoritmo de búsqueda hasta llegar a la posición.
+
