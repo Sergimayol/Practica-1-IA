818d50056c50619b9b7f450d62784c3fbe10acbf
Alejandro Rodríguez <alerodriargui@gmail.com>
2022-10-25 12:35:43 +0200

    update

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -6,12 +6,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 sys.path.append(str(BASE_DIR))
 
+from practica1 import agent_busq_no_informado
 from practica1 import agent, joc
 from practica1 import entorn as entorn_practica1
 
 
 def main():
-    rana = agent.Rana("Miquel")
+    rana = agent_busq_no_informado.Rana("Miquel")
     lab = joc.Laberint([rana], parets=True)
     pos_rana = rana.get_rana()
     pizza = lab.get_pizza()
