962a4e1e57d4393b36327335e1a6cbcc31f5af3b
unknown <sergi2808@gmail.com>
2022-10-29 12:17:39 +0200

    algoritmo minimax :+1:
    `Actua` no acabado

--- practica1/agent.py
+++ practica1/agent.py
@@ -11,29 +11,11 @@ import copy
 from practica1.entorn import ClauPercepcio
 
 
-
-class Rana(joc.Rana):
-    def __init__(self, *args, **kwargs):
-        super(Rana, self).__init__(*args, **kwargs)
-
-    def pinta(self, display):
-        pass
-
-    def actua(
-            self, percep: entorn.Percepcio
-    ) -> entorn.Accio | tuple[entorn.Accio, object]:
-        # Implmentar aquí lógica agente
-        # Estado inicial de la rana
-        estado_inicial = Estado(percep[entorn_practica1.ClauPercepcio.POSICIO], 0, padre=None)
-        estado_inicial.generar_hijos()
-        # EN un futuro cambiar, de momento es para que no de error
-        return entorn_practica1.AccionsRana.ESPERAR
-
 class Estado:
     def __init__(self, info: str, coste: int, padre=None):
-        self.__info = info   #[posicio, olor, parets]
-        self.__padre = padre # Estado
-        self.__coste = coste # Coste de las acciones
+        self.__info = info  # [posicio, olor, parets]
+        self.__padre = padre  # Estado
+        self.__coste = coste  # Coste de las acciones
 
     def __hash__(self) -> int:
         return hash(tuple(self.__info))
@@ -47,7 +29,7 @@ class Estado:
 
     def padre(self):
         return self.__padre
-    
+
     def __str__(self) -> str:
         return str(self.__info)
 
@@ -55,20 +37,20 @@ class Estado:
         # Comprobar si el movimiento es legal
         if [ClauPercepcio.PARETS] == entorn_practica1.Direccio.ESQUERRE:
             return False
-        
+
         if [ClauPercepcio.PARETS] == entorn_practica1.Direccio.DRETA:
             return False
 
         if [ClauPercepcio.PARETS] == entorn_practica1.Direccio.DALT:
             return False
-        
+
         if [ClauPercepcio.PARETS] == entorn_practica1.Direccio.BAIX:
             return False
         return True
-    
+
     # Método para generar hijos de un estado
     def generar_hijos(self):
-        hijos = [] # Lista de estados
+        hijos = []  # Lista de estados
         for columna in range(len(self.__info)):
             for fila in range(len(self.__info)):
                 if not self.es_meta():
@@ -77,7 +59,7 @@ class Estado:
 
                     print(estadoAux)
 
-        #print(hijos)
+        # print(hijos)
         return hijos
 
         # Primero para generar los hijos debemos saber el estado inicial y el estado final
@@ -85,3 +67,24 @@ class Estado:
         # es decir, si por ejemplo tenemos la pizza en la posición (0,0) y la rana en la posición (1,1) entonces sabemos que la rana debe igualar la posición de la pizza
         # y hará los movimientos basados en el algoritmo de búsqueda hasta llegar a la posición.
 
+    def puntuacion(self) -> int | None:
+        return self.__coste if self.es_meta() else None
+
+
+class Rana(joc.Rana):
+    def __init__(self, *args, **kwargs):
+        super(Rana, self).__init__(*args, **kwargs)
+
+    def pinta(self, display):
+        pass
+
+    def actua(
+            self, percep: entorn.Percepcio
+    ) -> entorn.Accio | tuple[entorn.Accio, object]:
+        # Implmentar aquí lógica agente
+        # Estado inicial de la rana
+        estado_inicial = Estado(
+            percep[entorn_practica1.ClauPercepcio.POSICIO], 0, padre=None)
+        estado_inicial.generar_hijos()
+        # EN un futuro cambiar, de momento es para que no de error
+        return entorn_practica1.AccionsRana.ESPERAR
--- practica1/agent_minimax.py
+++ practica1/agent_minimax.py
@@ -1,5 +1,49 @@
-from agent import Rana
+from agent import Rana, Estado
+from ia_2022 import entorn
+from practica1 import joc, entorn as entorn_practica1
+from practica1.entorn import ClauPercepcio
 
 
 class RanaMiniMax(Rana):
-    pass
+    def __init__(self):
+        super(RanaMiniMax, self).__init__()
+        self.__abiertos = None
+        self.__cerrados = None
+        self.__acciones = None
+
+    def pinta(self, display):
+        pass
+
+    def busqueda_minimax(self, estado: Estado, turno: bool = True) -> int:
+        """ Algoritmo de busqueda minimax
+            Min -> False
+            Args:
+                estado_inicial (Estado): Estado inicial del problema
+                turno (bool, optional): Turno de la rana. Defaults to True. Min -> False, Max -> True
+
+            Returns:
+                int: Valor de la mejor accion
+        """
+        puntuacion = estado.puntuacion()
+        if puntuacion != 0:
+            return puntuacion
+
+        puntuacion_hijos = [self.busqueda_minimax(
+            hijo, not turno) for hijo in estado.generar_hijos()]
+
+        return max(puntuacion_hijos) if turno else min(puntuacion_hijos)
+
+    def actua(
+            self, percep: entorn.Percepcio
+    ) -> entorn.Accio | tuple[entorn.Accio, object]:
+
+        estado_inicial = Estado(percep[ClauPercepcio.POSICIO], 0, padre=None)
+
+        if self.__acciones is None:
+            self.busqueda_minimax(estado_inicial)
+
+        if self.__acciones:
+            # Devolver la accion
+            pass
+
+        return entorn_practica1.AccionsRana.ESPERAR
