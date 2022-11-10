d0f3a8c11f8781da0b1ebc9db41fe53cf2b787bd
unknown <sergi2808@gmail.com>
2022-11-10 20:14:52 +0100

    initial dev commit

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -1,27 +1,10 @@
-import sys
-from pathlib import Path
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-sys.path.append(str(BASE_DIR))
-
-from practica1 import agent_busq_no_informado
-from practica1 import agent_minimax
 from practica1 import agent, joc
-from practica1 import entorn as entorn_practica1
 
 
 def main():
-    rana = agent_busq_no_informado.RanaBusquedaNoInformada("Miquel")
+    rana = agent.Rana("Miquel")
     lab = joc.Laberint([rana], parets=True)
-    pos_rana = rana.get_rana()
-    pizza = lab.get_pizza()
-    print(pizza)    # Posición estado inicial pizza
-    print(pos_rana) # Posición estado inicial rana
-    print("=========================================")
     lab.comencar()
-
 
 
 if __name__ == "__main__":
--- practica1/agent.py
+++ practica1/agent.py
@@ -6,10 +6,7 @@ ClauPercepcio:
     PARETS = 2
 """
 from ia_2022 import entorn
-from practica1 import joc, entorn as entorn_practica1
-import copy
-from practica1.entorn import ClauPercepcio, AccionsRana, Direccio
-
+from practica1 import joc
 
 
 class Rana(joc.Rana):
@@ -18,107 +15,8 @@ class Rana(joc.Rana):
 
     def pinta(self, display):
         pass
+
     def actua(
             self, percep: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
-        # Implmentar aquí lógica agente
-        # Estado inicial de la rana
-        estado_inicial = Estado(percep[ClauPercepcio.POSICIO], 0, padre=None)
-        # EN un futuro cambiar, de momento es para que no de error
-        return entorn_practica1.AccionsRana.ESPERAR
-
-class Estado:
-    def __init__(self,  info, coste: int, padre=None):
-        self.__info = info   #[posicio, olor, parets]
-        self.__padre = padre # Estado
-        self.__coste = coste # Coste de las acciones
-
-    def __hash__(self) -> int:
-        return hash(tuple(self.__info))
-    
-    @property
-    def info(self):
-        return self.__info
-
-    def es_meta(self) -> bool:
-        return self.__info == ClauPercepcio.OLOR
-
-    def padre(self):
-        return self.__padre
-    
-    def __str__(self) -> str:
-        return str(self.__info)
-
-    def legal(self) -> bool:
-        # Comprobar si el movimiento es legal
-        if [ClauPercepcio.PARETS] == Direccio.ESQUERRE:
-            return False
-        
-        if [ClauPercepcio.PARETS] == Direccio.DRETA:
-            return False
-
-        if [ClauPercepcio.PARETS] == Direccio.DALT:
-            return False
-        
-        if [ClauPercepcio.PARETS] == Direccio.BAIX:
-            return False
-        return True
-
-    def estado_inicial(self):
-        hijo = copy.deepcopy(self.__info)
-        posicion_inicial = hijo['Miquel']
-        return posicion_inicial
-    
-    # Método para generar hijos de un estado
-    def generar_hijos(self):
-        hijos = [] # Lista de estados
-        hijo = copy.deepcopy(self.__info) #Pasamos la información inicial del estado
-        print(hijo)
-        pos_inicial = (5,5)  #Se devuelve la tupla inicial
-        # Se generan los hijos
-        # Movimientos posibles
-        # Se comprueba si es legal
-        
-        #pos_inicial[0] = Columna
-        #pos_inicial[1] = Fila
-        # Se mueve a la derecha
-        if (pos_inicial[0] + 1, pos_inicial[1]) not in hijos:
-            #Si no está en la última columna
-            if (pos_inicial[0]<7):
-                #Añadimos el hijo
-                actual = (pos_inicial[0] + 1, pos_inicial[1])
-                print(actual)
-                hijos.append(Estado(actual, self.__coste + 1, (self, (AccionsRana.MOURE, Direccio.DRETA))))
-        # Se mueve a la izquierda
-        if (pos_inicial[0] - 1, pos_inicial[1]) not in hijos:
-            #Si no está en la primera columna
-            if (pos_inicial[0]>0):
-                #Añadimos el hijo
-                actual = (pos_inicial[0] - 1, pos_inicial[1])
-                print(actual)
-                hijos.append(Estado(actual, self.__coste + 1, (self, (AccionsRana.MOURE, Direccio.ESQUERRE))))
-        # Se mueve hacia arriba
-        if (pos_inicial[0], pos_inicial[1] - 1) not in hijos:
-            #Si no está en la primera fila
-            if (pos_inicial[1]>0):
-                #Añadimos el hijo
-                actual = (pos_inicial[0], pos_inicial[1] + 1)
-                print(actual)
-                hijos.append(Estado(actual, self.__coste + 1, (self, (AccionsRana.MOURE, Direccio.DALT))))
-        # Se mueve hacia abajo
-        if (pos_inicial[0], pos_inicial[1] + 1) not in hijos:
-            #Si no está en la ultima fila
-            if (pos_inicial[1] < 7):
-                #Añadimos el hijo
-                actual = (pos_inicial[0], pos_inicial[1] - 1)
-                print(actual)
-                hijos.append(Estado(actual, self.__coste + 1, (self, (AccionsRana.MOURE, Direccio.BAIX))))
-        print(hijos)
-        #Devolvemos los hijos
-        return hijos
-
-        # Primero para generar los hijos debemos saber el estado inicial y el estado final
-        # sabiendo el estado inicial de la pizza y el estado inicial de la rana, podemos calcular de la forma que nos convenga
-        # es decir, si por ejemplo tenemos la pizza en la posición (0,0) y la rana en la posición (1,1) entonces sabemos que la rana debe igualar la posición de la pizza
-        # y hará los movimientos basados en el algoritmo de búsqueda hasta llegar a la posición generando los hijos pertinentes.
-
+        pass
