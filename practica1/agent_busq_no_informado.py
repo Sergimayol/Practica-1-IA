197d1ab0ad8414f7030ea98e7d313424e974a860
alerodriargui <alerodriargui@gmail.com>
2022-11-12 15:19:49 +0100

    Comentado

--- practica1/agent_busq_no_informado.py
+++ practica1/agent_busq_no_informado.py
@@ -18,34 +18,50 @@ class RanaBusquedaNoInformada(Rana):
         Args:
             estado (Estado): Estado en el que se encuentra la rana.
         """
+        #Lista de estados abiertos
         self.__abiertos = []
+        #Conjunto de estados cerrados
         self.__cerrados = set()
-
+        #Añadimos el estado inicial a la lista de abiertos
         self.__abiertos.append(estado)
+        #Declaramos el estado actual como None
         estado_actual: Estado = None
+        #La profundidad actual es igual a la profundidad pasada por parámetro
         profundidad_actual = profundidad
+        #Mientras la lista de abiertos no esté vacía y la profundidad actual sea mayor o igual que 0
         while len(self.__abiertos) > 0 and profundidad_actual >= 0:
+            #Sacamos el último estado de la lista de abiertos
             estado_actual = self.__abiertos.pop()
+            #Si el estado actual está en la lista de cerrados
             if estado_actual in self.__cerrados:
+                #Seguimos
                 continue
-
+            #Si el estado actual es el estado meta
             if estado_actual.es_meta("Miquel"):
+                #Salimos del bucle
                 break
 
-            # Cambiar el nombre rana dinámico
+            # Se generan los estados hijos del estado actual
             estados_hijos = estado_actual.generar_hijos("Miquel")
 
+            #Para cada estado de los estados hijos
             for estado_hijo in estados_hijos:
+                #Añadimos el estado hijo a la lista de abiertos
                 self.__abiertos.append(estado_hijo)
-
+            #Añadimos el estado actual a la lista de cerrados
             self.__cerrados.add(estado_actual)
+            #Decrementamos la profundidad actual
             profundidad_actual -= 1
 
+        #Control de errores
         if estado_actual is None:
             raise ValueError("Error imposible")
 
-        if estado_actual.es_meta("Miquel"):
+        #Si el estado actual es el estado meta
+        if estado_actual.es_meta():
+            #Se declara una lista de acciones vacía
             acciones = []
+            #Se crea un iterador y pasamos el estado actual
             iterador = estado_actual
             while iterador.padre is not None:
                 padre, accion = iterador.padre
