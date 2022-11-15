262cd390fe330a6524a52b0dba5f39dc67bb15eb
unknown <sergi2808@gmail.com>
2022-11-15 22:37:11 +0100

    Update agent_minimax.py

--- practica1/agent_minimax.py
+++ practica1/agent_minimax.py
@@ -1,15 +1,16 @@
-from agent import Rana, Estado
+from agent import Rana
 from ia_2022 import entorn
 from practica1 import joc, entorn as entorn_practica1
 from practica1.entorn import ClauPercepcio, AccionsRana
 
 
+class Estado:
+    pass
+
+
 class RanaMiniMax(Rana):
     def __init__(self, *args, **kwargs):
         super(Rana, self).__init__(*args, **kwargs)
-        self.__abiertos = None
-        self.__cerrados = None
-        self.__acciones = None
 
     def pinta(self, display):
         pass
@@ -23,35 +24,9 @@ class RanaMiniMax(Rana):
         Returns:
             int: Valor de la mejor accion
         """
-        puntuacion = estado.puntuacion()
-        if puntuacion != 0:
-            return puntuacion
-
-        puntuacion_hijos = [
-            self.busqueda_minimax(hijo, not turno) for hijo in estado.generar_hijos()
-        ]
-
-        return max(puntuacion_hijos) if turno else min(puntuacion_hijos)
+        pass
 
     def actua(
         self, percep: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
-
-        estado_inicial = Estado(percep.to_dict(), 0, padre=None)
-
-        if self.__acciones is None:
-            self.busqueda_minimax(estado_inicial, True)
-
-        acciones = []
-        iterador = estado_inicial
-
-        while iterador.padre() is not None:
-            padre, accion = iterador.padre()
-            iterador = padre
-            acciones.append(accion)
-
-        if self.__acciones:
-            acc = self.__acciones.pop()
-            return acc[0], acc[1]
-
-        return AccionsRana.ESPERAR
+        pass
