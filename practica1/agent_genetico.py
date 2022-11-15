55add795549d866a6771b2eca4bd0e557e0f6139
unknown <sergi2808@gmail.com>
2022-11-15 19:11:53 +0100

    Update agent_genetico.py

--- practica1/agent_genetico.py
+++ practica1/agent_genetico.py
@@ -1,10 +1,257 @@
+import random
 from agent import Rana
-from practica1.agent import Estado
 from ia_2022 import entorn
-from practica1 import entorn as entorn_practica1
+from practica1.entorn import AccionsRana, ClauPercepcio, Direccio
+import copy
+
+
+class Estado:
+    def __init__(
+        self,
+        info: dict,
+        coste: int,
+        padre=None,
+        direccion: Direccio = None,
+        accion: AccionsRana = None,
+    ):
+        self.__info = info  # información sobre la rana
+        self.__padre = padre  # padre del estado
+        self.__coste = coste  # coste del estado
+        self.__direccion = direccion  # direccion del estado
+        self.__accion = accion  # accion del estado
+        self.__max_tablero = 7
+
+    def __hash__(self) -> int:
+        return hash(tuple(self.__info))
+
+    def get_direccion(self) -> Direccio or None:
+        return self.__direccion
+
+    def get_coste(self) -> int:
+        return self.__coste
+
+    def get_accion(self) -> AccionsRana or None:
+        return self.__accion
+
+    def get_fitness(self, nombre_rana: str) -> int:
+        # obtener la posicion de la rana
+        pos_rana = self.__info.get(ClauPercepcio.POSICIO).get(nombre_rana)
+        # obtener la posicion del olor
+        pos_olor = self.__info.get(ClauPercepcio.OLOR)
+        # calcular la distancia de Manhattan
+        return (
+            abs(pos_rana[0] - pos_olor[0])
+            + abs(pos_rana[1] - pos_olor[1])
+            + self.__coste
+        )
+
+    @property
+    def info(self):
+        return self.__info
+
+    def es_meta(self, nombre_rana: str) -> bool:
+        return self.__info.get(ClauPercepcio.POSICIO).get(
+            nombre_rana
+        ) == self.__info.get(ClauPercepcio.OLOR)
+
+    def get_posicion(self, nombre_rana: str) -> tuple:
+        return self.__info.get(ClauPercepcio.POSICIO).get(nombre_rana)
+
+    def get_comida(self):
+        return self.__info.get(ClauPercepcio.OLOR)
+
+    def get_len_tablero(self) -> int:
+        return self.__max_tablero
+
+    def set_direccion(self, direccion: Direccio):
+        self.__direccion = direccion
+
+    def set_accion(self, accion: AccionsRana):
+        self.__accion = accion
+
+    @property
+    def padre(self):
+        return self.__padre
+
+    @padre.setter
+    def padre(self, value):
+        self.__padre = value
+
+    def __getitem__(self, key):
+        if key == 0:
+            return self.__info.get(ClauPercepcio.POSICIO)
+        elif key == 1:
+            return self.__info.get(ClauPercepcio.OLOR)
+        elif key == 2:
+            return self.__info.get(ClauPercepcio.PARETS)
+
+    def __setitem__(self, key, value):
+        self.__info[key] = value
+
+    def __str__(self) -> str:
+        return (
+            str(self.__info)
+            + f" coste: {self.__coste}"
+            + f" accion: {self.__accion}"
+            + f" direccion: {self.__direccion}"
+        )
+
+    def __eq__(self, __o: object) -> bool:
+        return self.__info.get(ClauPercepcio.POSICIO) == __o.info.get(
+            ClauPercepcio.POSICIO
+        )
+
+    def __lt__(self, __o: object) -> bool:
+        return False
+        # return self.__coste < __o.get_coste()
+
+    def calcular_heuritica(self, nombre_rana) -> int:
+        # obtener la posicion de la rana
+        pos_rana = self.__info.get(ClauPercepcio.POSICIO).get(nombre_rana)
+        # obtener la posicion del olor
+        pos_olor = self.__info.get(ClauPercepcio.OLOR)
+        # calcular la distancia de Manhattan y añadir el coste
+        return (
+            abs(pos_rana[0] - pos_olor[0])
+            + abs(pos_rana[1] - pos_olor[1])
+            + self.__coste
+        )
+
+    def get_frog_names(self) -> list[str]:
+        return list(self.__info.get(ClauPercepcio.POSICIO).keys())
+
+    def legal(self, pos_actual: tuple) -> bool:
+        # obtener los muros
+        walls = self.__info.get(ClauPercepcio.PARETS)
+        # comprobar si la posicion actual esta en los muros
+        return pos_actual not in walls
+
+    def generar_hijos(self, nombre_rana: str) -> list:
+        """
+        Esta funcion genera los posibles estados hijos de un estado,
+        siempre y cuando sean legales.
+        Args:
+            nombre_rana (_str_): nombre de la rana que se desea generar los hijos
+        Returns:
+            _list_: list de estados hijos generados
+        """
+
+        hijos = []
+
+        # pos 0 = x, pos 1 = y (empiezan en 0)
+        pos_actual = self.__info.get(ClauPercepcio.POSICIO).get(nombre_rana)
+
+        if pos_actual[0] > 0:
+            # movimientos a la izquierda
+            new_pos = (pos_actual[0] - 1, pos_actual[1])
+            # Estado hijo a crear
+            estado_hijo: Estado = None
+            if self.legal(new_pos):
+                hijo = copy.deepcopy(self.__info)
+                hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
+                estado_hijo = Estado(
+                    hijo, self.__coste + 1, self, Direccio.ESQUERRE, AccionsRana.MOURE
+                )
+
+            if pos_actual[0] > 1:
+                new_pos = (pos_actual[0] - 2, pos_actual[1])
+                if self.legal(new_pos):
+                    hijo = copy.deepcopy(self.__info)
+                    hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
+                    estado_hijo = Estado(
+                        hijo,
+                        self.__coste + 2,
+                        self,
+                        Direccio.ESQUERRE,
+                        AccionsRana.BOTAR,
+                    )
+            if estado_hijo is not None:
+                hijos.append(estado_hijo)
+
+        if pos_actual[0] < self.__max_tablero:
+            # movimientos a la derecha
+            new_pos = (pos_actual[0] + 1, pos_actual[1])
+            # Estado hijo a crear
+            estado_hijo: Estado = None
+            if self.legal(new_pos):
+                hijo = copy.deepcopy(self.__info)
+                hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
+                estado_hijo = Estado(
+                    hijo, self.__coste + 1, self, Direccio.DRETA, AccionsRana.MOURE
+                )
+
+            if pos_actual[0] < self.__max_tablero - 1:
+                new_pos = (pos_actual[0] + 2, pos_actual[1])
+                if self.legal(new_pos):
+                    hijo = copy.deepcopy(self.__info)
+                    hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
+                    estado_hijo = Estado(
+                        hijo,
+                        self.__coste + 2,
+                        self,
+                        Direccio.DRETA,
+                        AccionsRana.BOTAR,
+                    )
+
+            if estado_hijo is not None:
+                hijos.append(estado_hijo)
+
+        if pos_actual[1] > 0:
+            # movimientos hacia arriba
+            new_pos = (pos_actual[0], pos_actual[1] - 1)
+            # Estado hijo a crear
+            estado_hijo: Estado = None
+            if self.legal(new_pos):
+                hijo = copy.deepcopy(self.__info)
+                hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
+                estado_hijo = Estado(
+                    hijo, self.__coste + 1, self, Direccio.DALT, AccionsRana.MOURE
+                )
+
+            if pos_actual[1] > 1:
+                new_pos = (pos_actual[0], pos_actual[1] - 2)
+                if self.legal(new_pos):
+                    hijo = copy.deepcopy(self.__info)
+                    hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
+                    estado_hijo = Estado(
+                        hijo, self.__coste + 2, self, Direccio.DALT, AccionsRana.BOTAR
+                    )
+
+            if estado_hijo is not None:
+                hijos.append(estado_hijo)
+
+        if pos_actual[1] < self.__max_tablero:
+            # movimientos hacia abajo
+            new_pos = (pos_actual[0], pos_actual[1] + 1)
+            # Estado hijo a crear
+            estado_hijo: Estado = None
+            if self.legal(new_pos):
+                hijo = copy.deepcopy(self.__info)
+                hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
+                estado_hijo = Estado(
+                    hijo, self.__coste + 1, self, Direccio.BAIX, AccionsRana.MOURE
+                )
+
+            if pos_actual[1] < self.__max_tablero - 1:
+                new_pos = (pos_actual[0], pos_actual[1] + 2)
+                if self.legal(new_pos):
+                    hijo = copy.deepcopy(self.__info)
+                    hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
+                    estado_hijo = Estado(
+                        hijo, self.__coste + 2, self, Direccio.BAIX, AccionsRana.BOTAR
+                    )
+
+            if estado_hijo is not None:
+                hijos.append(estado_hijo)
+
+        return hijos
 
 
 class RanaGenetica(Rana):
+
+    POBLACION_INICIAL = 1000
+    NUM_GENERACIONES = 1000
+
     def __init__(self, *args, **kwargs):
         super(Rana, self).__init__(*args, **kwargs)
         # Class to implement genetic algorithm
@@ -18,82 +265,101 @@ class RanaGenetica(Rana):
             estado (Estado): Estado inicial del problema.
         """
         # Genetic algorithm
-        # 1. Generate initial population
+        # 1. Generate initial population with random candidate solutions
         self._generar_poblacion(estado)
+        print("Población inicial generada")
+        for p in self.__poblacion:
+            print(p)
+        # 2. Evaluate each candidate
+        # self._evaluar_poblacion()
+        # 3. Repeat until a solution is found
         while True:
-            # 2. Evaluate population
-            self._evaluar_poblacion()
-            # 3. Select individuals
+            # 4. Select parents
+            dos_mejores_individuos = self._seleccion()
+            # 5. Recombine pair of parents
+            self._recombinar(dos_mejores_individuos)
+            # 6. Mutate
+            self._mutar()
+            # 7. Select individuals for next generation
             self._seleccion()
-            # 4. Create new population
-            self._crear_poblacion()
-            # 5. Check if solution is found
+            # 8. Check if solution is found
             if self._solucion_encontrada():
                 break
-        # 6. Repeat steps 2 to 5 until a solution is found
-        pass
 
     def _generar_poblacion(self, estado: Estado):
-        """Método que genera la población inicial.
+        """Genera la población inicial.
 
         Args:
             estado (Estado): Estado inicial del problema.
         """
-        pass
+        self.__poblacion = []
+        estado_hijo = estado.generar_hijos("Miquel")
+
+        for estado in estado_hijo:
+            self.__poblacion.append(estado)
 
     def _evaluar_poblacion(self):
-        """Método que evalúa la población."""
-        pass
+        """Evalua la población."""
+        for estado in self.__poblacion:
+            pass
 
-    def _fitness(self, individuo):
-        """Método que calcula el fitness de un individuo.
+    def _seleccion(self, nombre_rana: str) -> list:
+        """Selecciona a los dos mejores individuos de la población."""
+        padres = []
+        # Select parents with best fitness
+        for individuo in self.__poblacion:
+            if len(padres) < 2:
+                padres.append(individuo)
+            else:
+                if individuo.get_fitness(nombre_rana) < padres[0].get_fitness(
+                    nombre_rana
+                ):
+                    padres[0] = individuo
+                    continue
 
-        Args:
-            individuo (list): Individuo del que se quiere calcular el fitness.
+                if individuo.get_fitness(nombre_rana) < padres[1].get_fitness(
+                    nombre_rana
+                ):
+                    padres[1] = individuo
+                    continue
 
-        Returns:
-            float: Fitness del individuo.
-        """
-        pass
+        return padres
 
-    def _seleccion(self, poblacion):
-        """Método que implementa la selección de los individuos de la población.
+    def _recombinar(self, padres: list):
+        """Recombina los padres."""
 
-        Args:
-            poblacion (list): Población de la que se quiere seleccionar los individuos.
-
-        Returns:
-            list: Individuos seleccionados.
-        """
-        pass
-
-    def _cruce(self, individuo1, individuo2):
-        """Método que implementa el cruce de dos individuos.
-
-        Args:
-            individuo1 (list): Primer individuo.
-            individuo2 (list): Segundo individuo.
-
-        Returns:
-            list: Individuo resultante del cruce.
-        """
-        pass
-
-    def _mutacion(self, individuo):
-        """Método que implementa la mutación de un individuo.
-
-        Args:
-            individuo (list): Individuo que se quiere mutar.
-
-        Returns:
-            list: Individuo mutado.
-        """
         pass
 
     def actua(
         self, percep: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
         # Estado inicial de la rana
-        estado_inicial = Estado(percep.to_dict(), 0, padre=None)
+        estado_inicial = Estado(
+            percep.to_dict(),
+            0,
+            padre=None,
+            accion=AccionsRana.ESPERAR,
+            direccion=None,
+        )
 
-        return entorn_practica1.AccionsRana.ESPERAR
+        if self.__acciones is None:
+            self._buscar(estado_inicial)
+            print("Acciones: ", self.__acciones)
+
+        if len(self.__acciones) == 0:
+            return AccionsRana.ESPERAR
+
+        if self.__saltando > 0:
+            self.__saltando -= 1
+            return AccionsRana.ESPERAR
+
+        accion = self.__acciones.pop()
+
+        if accion[1] is None:
+            return accion[0]
+
+        if accion[0] == AccionsRana.BOTAR:
+            self.__saltando = 2
+            return accion[0], accion[1]
+
+        return accion[0], accion[1]
