80336fbb7b540a50ff3dd6f56589db209ef66aed
Sergi Mayol <sergi2808@gmail.com>
2022-11-16 09:38:04 +0100

    Update agent_genetico.py

--- practica1/agent_genetico.py
+++ practica1/agent_genetico.py
@@ -5,249 +5,50 @@ from practica1.entorn import AccionsRana, ClauPercepcio, Direccio
 import copy
 
 
-class Estado:
-    COSTE_MOVERSE = 1
-    COSTE_ESPERAR = 0.5
-    COSTE_SALTAR = 6
-
+class Individuo:
     def __init__(
         self,
-        info: dict,
-        coste: int,
-        padre=None,
+        posicion: tuple = None,
         direccion: Direccio = None,
-        accion: AccionsRana = None,
+        padres=None,
+        poscion_meta: tuple = None,
     ):
-        self.__info = info  # información sobre la rana
-        self.__padre = padre  # padre del estado
-        self.__coste = coste  # coste del estado
-        self.__direccion = direccion  # direccion del estado
-        self.__accion = accion  # accion del estado
-        self.__max_tablero = 7
-
-    def __hash__(self) -> int:
-        return hash(tuple(self.__info))
-
-    def get_direccion(self) -> Direccio or None:
-        return self.__direccion
-
-    def get_coste(self) -> int:
-        return self.__coste
-
-    def get_accion(self) -> AccionsRana or None:
-        return self.__accion
-
-    def get_fitness(self, nombre_rana: str) -> int:
-        # obtener la posicion de la rana
-        pos_rana = self.__info.get(ClauPercepcio.POSICIO).get(nombre_rana)
-        # obtener la posicion del olor
-        pos_olor = self.__info.get(ClauPercepcio.OLOR)
-        # calcular la distancia de Manhattan
-        return (
-            abs(pos_rana[0] - pos_olor[0])
-            + abs(pos_rana[1] - pos_olor[1])
-            + self.__coste
-        )
-
-    @property
-    def info(self):
-        return self.__info
-
-    def es_meta(self, nombre_rana: str) -> bool:
-        return self.__info.get(ClauPercepcio.POSICIO).get(
-            nombre_rana
-        ) == self.__info.get(ClauPercepcio.OLOR)
-
-    def get_len_tablero(self) -> int:
-        return self.__max_tablero
-
-    def set_direccion(self, direccion: Direccio):
+        self.__posicion = posicion
         self.__direccion = direccion
-
-    def set_accion(self, accion: AccionsRana):
-        self.__accion = accion
-
-    @property
-    def padre(self):
-        return self.__padre
-
-    @padre.setter
-    def padre(self, value):
-        self.__padre = value
-
-    def __getitem__(self, key):
-        if key == 0:
-            return self.__info.get(ClauPercepcio.POSICIO)
-        elif key == 1:
-            return self.__info.get(ClauPercepcio.OLOR)
-        elif key == 2:
-            return self.__info.get(ClauPercepcio.PARETS)
-
-    def __setitem__(self, key, value):
-        self.__info[key] = value
+        self.__padres = padres
+        self.__poscion_meta = poscion_meta
+        self.__fitness = 0
 
     def __str__(self) -> str:
-        return f"Estado: {self.__info}, Coste: {self.__coste}, Accion: {self.__accion}, Direccion: {self.__direccion}"
+        return f"Individuo: {self.__posicion} {self.__direccion} {self.__padres} {self.__poscion_meta} {self.__fitness}"
+
+    def __hash__(self):
+        return hash(tuple(self.__posicion))
 
     def __eq__(self, __o: object) -> bool:
-        return self.__info.get(ClauPercepcio.POSICIO) == __o.info.get(
-            ClauPercepcio.POSICIO
-        )
+        return self.__posicion == __o.__posicion
 
     def __lt__(self, __o: object) -> bool:
-        return False
-        # return self.__coste < __o.get_coste()
+        return self.__fitness < __o.__fitness
 
-    def legal(self, pos_actual: tuple) -> bool:
-        # obtener los muros
-        walls = self.__info.get(ClauPercepcio.PARETS)
-        # comprobar si la posicion actual esta en los muros
-        return pos_actual not in walls
+    def cruzar(self, otro):
+        pass
 
-    def generar_hijos(self, nombre_rana: str) -> list:
-        """
-        Esta funcion genera los posibles estados hijos de un estado,
-        siempre y cuando sean legales.
-        Args:
-            nombre_rana (_str_): nombre de la rana que se desea generar los hijos
-        Returns:
-            _list_: list de estados hijos generados
-        """
-        hijos = []
-        # pos 0 = x, pos 1 = y (empiezan en 0)
-        pos_actual = self.__info.get(ClauPercepcio.POSICIO).get(nombre_rana)
+    def mutar(self):
+        pass
 
-        if pos_actual[0] > 0:
-            # movimientos a la izquierda
-            new_pos = (pos_actual[0] - 1, pos_actual[1])
-            if self.legal(new_pos):
-                hijo = copy.deepcopy(self.__info)
-                hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
-                hijos.append(
-                    Estado(
-                        hijo,
-                        self.COSTE_MOVERSE,
-                        self,
-                        Direccio.ESQUERRE,
-                        AccionsRana.MOURE,
-                    )
-                )
-
-            if pos_actual[0] > 1:
-                new_pos = (pos_actual[0] - 2, pos_actual[1])
-                if self.legal(new_pos):
-                    hijo = copy.deepcopy(self.__info)
-                    hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
-                    hijos.append(
-                        Estado(
-                            hijo,
-                            self.COSTE_SALTAR,
-                            self,
-                            Direccio.ESQUERRE,
-                            AccionsRana.BOTAR,
-                        )
-                    )
-
-        if pos_actual[0] < self.__max_tablero:
-            # movimientos a la derecha
-            new_pos = (pos_actual[0] + 1, pos_actual[1])
-            if self.legal(new_pos):
-                hijo = copy.deepcopy(self.__info)
-                hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
-                hijos.append(
-                    Estado(
-                        hijo,
-                        self.COSTE_MOVERSE,
-                        self,
-                        Direccio.DRETA,
-                        AccionsRana.MOURE,
-                    )
-                )
-
-            if pos_actual[0] < self.__max_tablero - 1:
-                new_pos = (pos_actual[0] + 2, pos_actual[1])
-                if self.legal(new_pos):
-                    hijo = copy.deepcopy(self.__info)
-                    hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
-                    hijos.append(
-                        Estado(
-                            hijo,
-                            self.COSTE_SALTAR,
-                            self,
-                            Direccio.DRETA,
-                            AccionsRana.BOTAR,
-                        )
-                    )
-
-        if pos_actual[1] > 0:
-            # movimientos hacia arriba
-            new_pos = (pos_actual[0], pos_actual[1] - 1)
-            if self.legal(new_pos):
-                hijo = copy.deepcopy(self.__info)
-                hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
-                hijos.append(
-                    Estado(
-                        hijo, self.COSTE_MOVERSE, self, Direccio.DALT, AccionsRana.MOURE
-                    )
-                )
-
-            if pos_actual[1] > 1:
-                new_pos = (pos_actual[0], pos_actual[1] - 2)
-                if self.legal(new_pos):
-                    hijo = copy.deepcopy(self.__info)
-                    hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
-                    hijos.append(
-                        Estado(
-                            hijo,
-                            self.COSTE_SALTAR,
-                            self,
-                            Direccio.DALT,
-                            AccionsRana.BOTAR,
-                        )
-                    )
-
-        if pos_actual[1] < self.__max_tablero:
-            # movimientos hacia abajo
-            new_pos = (pos_actual[0], pos_actual[1] + 1)
-            if self.legal(new_pos):
-                hijo = copy.deepcopy(self.__info)
-                hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
-                hijos.append(
-                    Estado(
-                        hijo, self.COSTE_MOVERSE, self, Direccio.BAIX, AccionsRana.MOURE
-                    )
-                )
-
-            if pos_actual[1] < self.__max_tablero - 1:
-                new_pos = (pos_actual[0], pos_actual[1] + 2)
-                if self.legal(new_pos):
-                    hijo = copy.deepcopy(self.__info)
-                    hijo[ClauPercepcio.POSICIO][nombre_rana] = new_pos
-                    hijos.append(
-                        Estado(
-                            hijo,
-                            self.COSTE_SALTAR,
-                            self,
-                            Direccio.BAIX,
-                            AccionsRana.BOTAR,
-                        )
-                    )
-
-        return hijos
+    def calcular_fitness(self):
+        pass
 
 
 class RanaGenetica(Rana):
-
-    POBLACION_INICIAL = 1000
-    NUM_GENERACIONES = 1000
-
     def __init__(self, *args, **kwargs):
         super(Rana, self).__init__(*args, **kwargs)
         # Class to implement genetic algorithm
         self.__poblacion = None
         self.__acciones = None
 
-    def _buscar(self, estado: Estado):
+    def _buscar(self, estado):
         """Método que implementa el algoritmo genético.
 
         Args:
@@ -285,7 +86,7 @@ class RanaGenetica(Rana):
         """
         pass
 
-    def _generar_poblacion(self, estado: Estado):
+    def _generar_poblacion(self, estado):
         """Genera la población inicial.
 
         Args:
@@ -346,16 +147,15 @@ class RanaGenetica(Rana):
         self, percep: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
         # Estado inicial de la rana
-        estado_inicial = Estado(
-            percep.to_dict(),
-            0,
-            padre=None,
-            accion=AccionsRana.ESPERAR,
-            direccion=None,
+        individuo_inicial = Individuo(
+            posicion=percep.posicio,
+            direccion=percep.direccio,
+            padres=None,
+            poscion_meta=percep.meta,
         )
 
         if self.__acciones is None:
-            self._buscar(estado_inicial)
+            self._buscar(individuo_inicial)
             print("Acciones: ", self.__acciones)
 
         if len(self.__acciones) == 0:
