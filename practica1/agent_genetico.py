446e0df173d81480bb44ff4e5a32c7d2d584c319
Sergi Mayol <sergi2808@gmail.com>
2022-11-17 18:34:08 +0100

    Update agent_genetico.py

--- practica1/agent_genetico.py
+++ practica1/agent_genetico.py
@@ -6,31 +6,45 @@ import copy
 
 
 class Individuo:
-    def __init__(self, acciones: list, fitness: float = 0, padre=None):
+    LISTA_ACCIONES = [AccionsRana.BOTAR, AccionsRana.ESPERAR, AccionsRana.MOURE]
+    LISTA_DIRECCIONES = [
+        Direccio.BAIX,
+        Direccio.DRETA,
+        Direccio.DRETA,
+        Direccio.ESQUERRE,
+    ]
+
+    def __init__(self, acciones: list, fitness: float = 0, padre: tuple = None):
         self.acciones = acciones
         self.fitness = fitness
         self.padre = padre
 
     def cruzar(self, otro):
-        pass
+        random_index = random.randint(0, len(self.acciones) - 1)
+        acciones = self.acciones[:random_index] + otro.acciones[random_index:]
+        return Individuo(acciones, padre=(self, otro))
 
-    def mutar(self):
-        pass
+    def mutar(self) -> None:
+        acciones = copy.deepcopy(self.acciones)
+        acciones[random.randint(0, len(acciones) - 1)] = (
+            random.choice(self.LISTA_ACCIONES),
+            random.choice(self.LISTA_DIRECCIONES),
+        )
+        self.acciones = acciones
 
     def calcular_fitness(self):
         pass
 
     def generar_acciones_aleatorias(self):
-        acciones = [AccionsRana.BOTAR, AccionsRana.ESPERAR, AccionsRana.MOURE]
-        direcciones = [Direccio.BAIX, Direccio.DRETA, Direccio.DRETA, Direccio.ESQUERRE]
-        return [(random.choice(acciones), random.choice(direcciones)) for _ in range(8)]
+        return [
+            (random.choice(self.LISTA_ACCIONES), random.choice(self.LISTA_DIRECCIONES))
+            for _ in range(8)
+        ]
 
 
 class RanaGenetica(Rana):
     def __init__(self, *args, **kwargs):
         super(Rana, self).__init__(*args, **kwargs)
-        self.__abiertos = None
-        self.__cerrados = None
         self.__acciones = None
         self.__saltando = 0
 
