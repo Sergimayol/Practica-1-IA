0c58836544a8909651389887efb1bf317e397b5a
unknown <sergi2808@gmail.com>
2022-11-21 22:25:03 +0100

    Update agent_a_estrella.py

--- practica1/agent_a_estrella.py
+++ practica1/agent_a_estrella.py
@@ -1,7 +1,7 @@
 from queue import PriorityQueue
-from agent import Rana
 from ia_2022 import entorn
 from practica1.entorn import AccionsRana, ClauPercepcio, Direccio
+from practica1 import joc
 import copy
 
 
@@ -251,13 +251,16 @@ class Estado:
         return hijos
 
 
-class RanaEstrella(Rana):
+class RanaEstrella(joc.Rana):
     def __init__(self, *args, **kwargs):
-        super(Rana, self).__init__(*args, **kwargs)
+        super(RanaEstrella, self).__init__(*args, **kwargs)
         self.__abiertos = None
         self.__cerrados = None
         self.__acciones = None
         self.__saltando = 0
+
+    def pinta(self, display):
+        pass
 
     def _buscar(self, estado: Estado, nombre_rana: str) -> bool:
         """Método que implementa el algoritmo de búsqueda A*.
